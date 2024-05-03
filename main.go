a1a814e1b24dde820193daba311f04759150c8d5
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-05-03 21:58:37 +0500

    Refactor main.go to initialize and start multiple servers

--- main.go
+++ main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"log"
 	"net"
-	p2p "poker/p2p"
+	"poker/p2p"
+	"poker/server"
+	"time"
 )
 
-func main() {
-	s := p2p.NewTCPTransport(p2p.TCPTransportOpts{
+func initServer(version string, variant server.GameVariant, port int) *server.Server {
+	t := p2p.NewTCPTransport(p2p.TCPTransportOpts{
 		Laddr: &net.TCPAddr{
 			IP:   net.ParseIP("127.0.0.1"),
-			Port: 3000,
+			Port: port,
 		},
 	})
-	if err := s.ListenAndAccept(); err != nil {
-		panic(err)
+
+	opts := &server.ServerConfig{
+		Transport:   t,
+		GameVariant: variant,
+		GameVersion: version,
+	}
+
+	return server.NewServer(opts)
+}
+
+func main() {
+	s1 := initServer("GGPOKE V0.1-alpha", server.TexasHoldings, 3000)
+	s2 := initServer("GGPOKE V0.1-alpha", server.TexasHoldings, 4000)
+	go func() {
+		log.Fatal(s1.Start())
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	go func() {
+		log.Fatal(s2.Start())
+	}()
+
+	time.Sleep(1 * time.Second)
+	if err := s2.Transport.Dial(3000); err != nil {
+		log.Println(err)
 	}
 	select {}
 }
