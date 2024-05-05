78bdb804c96f1ee3c988682320f0c4f9d617aa4c
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-05-05 21:56:11 +0500

    implement handshake b/w peers

---
+++ p2p/handshake.go
@@ -0,0 +1,53 @@
+package p2p
+
+import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+	"log/slog"
+)
+
+type HandshakeFunc func(*TCPPeer, GameVariant, string) error
+
+type HandshakePass struct {
+	GameVariant GameVariant
+	Version     string
+}
+
+func PerformHandshake(p *TCPPeer, variant GameVariant, version string) error {
+	slog.Info("Performing Handshake")
+	slog.Info("Connection Established", "RemoteAddr", p.conn.RemoteAddr())
+	hp := &HandshakePass{}
+	// blocks here
+	if err := gob.NewDecoder(p.conn).Decode(hp); err != nil {
+		return err
+	}
+
+	slog.Info("Received Handshake", "GameVariant", hp.GameVariant, "Version", hp.Version)
+	if hp.GameVariant != variant {
+		slog.Error("Game Variant Mismatch", "Expected", variant, "Received", hp.GameVariant)
+		return errors.ErrUnsupported
+	}
+
+	if hp.Version != version {
+		slog.Error("Version Mismatch", "Expected", version, "Received", hp.Version)
+		return errors.ErrUnsupported
+	}
+
+	slog.Info("Handshake Performed", "GameVariant", hp.GameVariant, "Version", hp.Version)
+	return nil
+}
+
+func SendHandshake(p *TCPPeer, variant GameVariant, version string) error {
+	hp := &HandshakePass{
+		GameVariant: variant,
+		Version:     version,
+	}
+
+	buf := new(bytes.Buffer)
+	if err := gob.NewEncoder(buf).Encode(hp); err != nil {
+		return err
+	}
+
+	return p.Send(buf.Bytes())
+}
