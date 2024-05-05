11c7596598cbb7ac5d133e19ce4af81d6f2a2a5e
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-05-05 15:13:44 +0500

    Add GameVariant type and String method to deck.go

--- deck/deck.go
+++ deck/deck.go
@@ -5,6 +5,24 @@ import (
 	"math/rand"
 )
 
+type GameVariant uint8
+
+func (gv GameVariant) String() string {
+	switch gv {
+	case TexasHoldings:
+		return "TEXAS HOLDINGS"
+	case Other:
+		return "other"
+	default:
+		return "unknown"
+	}
+}
+
+const (
+	TexasHoldings GameVariant = iota
+	Other
+)
+
 type Suit int
 
 func (s Suit) String() string {
