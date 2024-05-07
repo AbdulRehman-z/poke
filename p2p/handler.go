c5fd49f22adafb7889e686d9d4a5d46e32d45acb
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-05-07 18:17:32 +0500

    Refactor p2p/handler.go to comment out unused code

--- p2p/handler.go
+++ p2p/handler.go
@@ -1,22 +1,22 @@
 package p2p
 
-import (
-	"io"
-	"log/slog"
-)
+// import (
+// 	"io"
+// 	"log/slog"
+// )
 
-type Handler interface {
-	HandleMessage(msg *Message) error
-}
+// type Handler interface {
+// 	HandleMessage(msg *Message) error
+// }
 
-type DefaultHandler struct{}
+// type DefaultHandler struct{}
 
-func (h *DefaultHandler) HandleMessage(msg *Message) error {
-	b, err := io.ReadAll(msg.Payload)
-	if err != nil {
-		return err
-	}
+// // func (h *DefaultHandler) HandleMessage(msg *Message) error {
+// // 	b, err := io.ReadAll(msg.Payload)
+// // 	if err != nil {
+// // 		return err
+// // 	}
 
-	slog.Info("Received message", "from", msg.From, "to", msg.To, "payload", string(b))
-	return nil
-}
+// // 	slog.Info("Received message", "from", msg.From, "to", msg.To, "payload", string(b))
+// // 	return nil
+// // }
