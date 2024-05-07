d3e76a519193811ecae8e924b63909edbf16c40a
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-05-07 18:17:20 +0500

    Refactor Handshake logging in PerformHandshake function and update HandshakeFunc signature

--- p2p/handshake.go
+++ p2p/handshake.go
@@ -4,17 +4,18 @@ import (
 	"bytes"
 	"encoding/gob"
 	"errors"
+	"fmt"
 	"log/slog"
 )
 
-type HandshakeFunc func(*TCPPeer, GameVariant, string) error
+type HandshakeFunc func(*TCPPeer, GameVariant, string, string) error
 
 type HandshakePass struct {
 	GameVariant GameVariant
 	Version     string
 }
 
-func PerformHandshake(p *TCPPeer, variant GameVariant, version string) error {
+func PerformHandshake(p *TCPPeer, variant GameVariant, version string, addr string) error {
 	// slog.Info("Performing Handshake")
 	hp := &HandshakePass{}
 	if err := gob.NewDecoder(p.conn).Decode(hp); err != nil {
@@ -32,7 +33,7 @@ func PerformHandshake(p *TCPPeer, variant GameVariant, version string) error {
 		return errors.ErrUnsupported
 	}
 
-	slog.Info("Handshake Performed", "GameVariant", hp.GameVariant, "Version", hp.Version, "peer", p.outbound)
+	fmt.Printf("%s has successfully performed handshake with %s\n", addr, p.conn.RemoteAddr())
 	return nil
 }
 
@@ -44,7 +45,8 @@ func SendHandshake(p *TCPPeer, variant GameVariant, version string) error {
 
 	buf := new(bytes.Buffer)
 	if err := gob.NewEncoder(buf).Encode(hp); err != nil {
-		return err
+		slog.Error("ERR send handshake", err, p.conn.RemoteAddr())
+		// return err
 	}
 
 	return p.Send(buf.Bytes())
