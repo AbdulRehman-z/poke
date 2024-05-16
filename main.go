b550893540b5f1ee4680d68fda5de3e0950d0802
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-05-16 19:48:26 +0500

    configured api

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"time"
 
-	"github.com/anthdm/ggpoker/p2p"
+	"github.com/AbdulRehman-z/poke/p2p"
 )
 
-func makeServerAndStart(addr string) *p2p.Server {
+func makeServerAndStart(addr string, apiAddr string) *p2p.Server {
 	cfg := p2p.ServerConfig{
-		Version:     "GGPOKER V0.1-alpha",
-		ListenAddr:  addr,
-		GameVariant: p2p.TexasHoldem,
+		Version:       "GGPOKER V0.1-alpha",
+		ListenAddr:    addr,
+		ApiListenAddr: apiAddr,
+		GameVariant:   p2p.TexasHoldem,
 	}
 	server := p2p.NewServer(cfg)
 	go server.Start()
@@ -21,10 +22,10 @@ func makeServerAndStart(addr string) *p2p.Server {
 }
 
 func main() {
-	playerA := makeServerAndStart(":3000")
-	playerB := makeServerAndStart(":4000")
-	playerC := makeServerAndStart(":5000")
-	playerD := makeServerAndStart(":7000")
+	playerA := makeServerAndStart(":3000", ":3001")
+	playerB := makeServerAndStart(":4000", ":4001")
+	playerC := makeServerAndStart(":5000", ":5001")
+	playerD := makeServerAndStart(":6000", ":6001")
 	//playerE := makeServerAndStart(":8000")
 
 	time.Sleep(time.Millisecond * 200)
