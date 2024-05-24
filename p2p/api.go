241e5104a87c052e2d681b68de8740078ebe3cb8
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-05-24 20:18:31 +0500

    add endpoints & refactor

--- p2p/api.go
+++ p2p/api.go
@@ -3,6 +3,7 @@ package p2p
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 type apiFunc func(http.ResponseWriter, *http.Request) error
@@ -23,29 +24,50 @@ func writeJson(w http.ResponseWriter, status int, v any) error {
 type ApiServer struct {
 	listenAddr string
 	game       *Game
+	mux        *http.ServeMux
 }
 
 func NewApiServer(addr string, game *Game) *ApiServer {
 	return &ApiServer{
 		listenAddr: addr,
 		game:       game,
+		mux:        http.NewServeMux(),
 	}
 }
 
 func (s *ApiServer) Run() {
-	mux := http.NewServeMux()
+	s.mux.HandleFunc("GET /ready", makeHttpHandleFunc(s.handlePlayersReady))
+	s.mux.HandleFunc("GET /fold", makeHttpHandleFunc(s.handlePlayersFold))
+	s.mux.HandleFunc("GET /check", makeHttpHandleFunc(s.handlePlayersChecked))
+	s.mux.HandleFunc("GET /bet/{value}", makeHttpHandleFunc(s.handlePlayersBet))
 
-	mux.HandleFunc("GET /ready", makeHttpHandleFunc(s.handlePlayersReady))
-	mux.HandleFunc("GET /fold", makeHttpHandleFunc(s.handlePlayersFold))
-	http.ListenAndServe(s.listenAddr, mux)
+	http.ListenAndServe(s.listenAddr, s.mux)
 }
 
 func (s *ApiServer) handlePlayersReady(w http.ResponseWriter, r *http.Request) error {
 	s.game.SetReady()
-	return writeJson(w, http.StatusOK, map[string]any{"Game server": s.game.currentStatus})
+	return writeJson(w, http.StatusOK, "READY")
+}
+
+func (s *ApiServer) handlePlayersBet(w http.ResponseWriter, r *http.Request) error {
+	val := r.PathValue("value")
+	intVal, _ := strconv.Atoi(val)
+	if err := s.game.TakeAction(PlayerActionBet, intVal); err != nil {
+		return err
+	}
+	return writeJson(w, http.StatusOK, "READY")
+}
+
+func (s *ApiServer) handlePlayersChecked(w http.ResponseWriter, r *http.Request) error {
+	if err := s.game.TakeAction(PLayerActionCheck, 0); err != nil {
+		return err
+	}
+	return writeJson(w, http.StatusOK, "CHECKED")
 }
 
 func (s *ApiServer) handlePlayersFold(w http.ResponseWriter, r *http.Request) error {
-	s.game.Fold()
-	return writeJson(w, http.StatusOK, map[string]any{"Game server": s.game.currentStatus})
+	if err := s.game.TakeAction(PlayerActionFold, 0); err != nil {
+		return err
+	}
+	return writeJson(w, http.StatusOK, "FOLDED")
 }
